57e8caa66df85c76911464771e3f17aa573fbb77
Maggie Lively <maggielively@maggielvelyspro.lan>
2022-12-07 10:33:56 -0500

    basic folder structure sample

---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
+}
